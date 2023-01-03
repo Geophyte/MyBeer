c739c7ca82429dc1e7f73d4d1e5127d9ce972652
Gephyte <idab@vp.pl>
2023-01-03 10:47:44 +0100

    reverse merge

--- backend/beers/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-from beers.models import Beer, Review, Comment, Category
-
-admin.site.register(Beer)
-admin.site.register(Review)
-admin.site.register(Comment)
-admin.site.register(Category)
-
